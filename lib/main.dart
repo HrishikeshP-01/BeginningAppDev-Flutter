74009e7535e2bf8f5f1ef2bafaf8a23e5bc49336
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-14 17:52:21 +0530

    Ch4 - Learninga about image widget - pg50

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(TestingIcons());
+void main()=>runApp(LearningImageWidget());
 
-class TestingIcons extends StatelessWidget
+/*
+* When adding an image to the assets folder, add it to pubspec.yaml & USE PROPER INDENDATION
+* Then run the cmd: flutter pub get
+* in the terminal
+* */
+
+class LearningImageWidget extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return MaterialApp(
-      home:Icon(
-        Icons.email,
-        color:Colors.green,
-        size:200,
+      home:Scaffold(
+        body:Column(
+          children: <Widget>[
+            Expanded(child:Image.network("https://avatars.githubusercontent.com/u/62914161?v=4")),
+            Expanded(child:Image.asset("assets/images/Fig3.png")),
+          ],
+        ),
       ),
     );
   }
