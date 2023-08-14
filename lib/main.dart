06d0e1b9ce7859977439feefa2e994ffcd28adb1
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-15 00:32:53 +0530

    Ch4 - Sizing an image - pg54

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,15 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(LearningImageWidget());
+void main()=>runApp(SizingImages());
 
-/*
-* When adding an image to the assets folder, add it to pubspec.yaml & USE PROPER INDENDATION
-* Then run the cmd: flutter pub get
-* in the terminal
-* */
-
-class LearningImageWidget extends StatelessWidget
+class SizingImages extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return MaterialApp(
-      home:Scaffold(
-        body:Column(
-          children: <Widget>[
-            Expanded(child:Image.network("https://avatars.githubusercontent.com/u/62914161?v=4")),
-            Expanded(child:Image.asset("assets/images/Fig3.png")),
-          ],
-        ),
-      ),
+      home:Image.asset("assets/images/Fig3.png",
+      fit:BoxFit.contain),
     );
   }
 }
