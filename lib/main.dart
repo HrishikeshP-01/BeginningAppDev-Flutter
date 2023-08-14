86419e278545b3b461ff3d07778910511874fc5a
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-14 17:29:59 +0530

    Ch3 - Passing name as value into widget - pg44

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(FancyHelloWorld());
+void main()=>runApp(HelloWorld());
 
-class FancyHelloWorld extends StatelessWidget
+class HelloWorld extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
@@ -10,17 +10,27 @@ class FancyHelloWorld extends StatelessWidget
     return MaterialApp(
       home:Scaffold(
         appBar:AppBar(
-          title:Text("Fancy Hello World"),
+          title:Text("Hello world app"),
         ),
         body:Container(
           alignment:Alignment.center,
-          child:Text("Hrishi says Hello World"),
-        ),
-        floatingActionButton:FloatingActionButton(
-          child:Icon(Icons.thumb_up),
-          onPressed:()=>{},
+          child:TextOfApp("Hrishikesh","P"),
         ),
       ),
     );
+  }
+}
+
+class TextOfApp extends StatelessWidget
+{
+  final String firstName;
+  final String lastName;
+
+  TextOfApp(this.firstName,this.lastName);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Text("$firstName $lastName says Hello World");
   }
 }
