6f9097f78e1d69801d17c165d80b6d84b49c69e3
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-18 01:47:02 +0530

    Ch5 - Icon button - pg79

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,19 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:SimpleForm()));
+void main()=>runApp(MaterialApp(home:Scaffold(body:IconButtonExample())));
 
-class SimpleForm extends StatefulWidget
+class IconButtonExample extends StatelessWidget
 {
+  void _buttonPressed()
+  {
+    print("Pressed button");
+  }
   @override
-  _State createState()=>_State();
-}
-
-class _State extends State<SimpleForm>
-{
-   GlobalKey<FormState> _key=GlobalKey<FormState>();
-
-   @override
   Widget build(BuildContext context)
-   {
-     return Scaffold(
-       body: Form(
-         key: _key,
-         autovalidate: true,
-         child: TextFormField(
-           decoration:InputDecoration(
-             label: Text("Name"),
-           ),
-           validator:(String? val){
-             if(val==null){
-               print("Null");
-               return "We need something to search for";
-             }
-             return null;
-           }
-         ),
-       ),
-     );
-   }
+  {
+    return IconButton(
+      icon:Icon(Icons.delete),
+      onPressed: _buttonPressed, // fn must return void
+    );
+  }
 }
