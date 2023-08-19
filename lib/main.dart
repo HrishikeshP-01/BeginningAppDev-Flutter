14485e95695fb90d86dee76398bc57b74c23c37a
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-19 10:07:52 +0530

    Ch5 - Dismissible button - pg83

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import "package:flutter/material.dart";
 import "package:flutter/cupertino.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:CupertinoExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:DismissibleExample())));
 
-class CupertinoExample extends StatelessWidget
+class DismissibleExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    return CupertinoButton(
-      child:Text("Cupertino button(iOS)"),
-      onPressed:()=>{print("Cupertino button pressed")},
+    return Dismissible(
+      child:Text("Dismissible"),
+      background:Container(color:Colors.red),
+      secondaryBackground:Container(color:Colors.blue),
+      key:Key(""),
     );
   }
 }
