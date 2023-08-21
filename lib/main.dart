5fc3940c895cb91a089cad8ef0131394b5122107
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-21 23:32:14 +0530

    Ch6 - Traffic appbar - pg106

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(
-  home:Container(
-    child:Text("Material app example"),
-  ),
-  title:"Material App Title",
-  theme:ThemeData(primarySwatch:Colors.purple),
-));
+void main()=>runApp(
+  MaterialApp(home:Scaffold(
+    appBar: AppBar(
+      leading:Icon(Icons.traffic),
+      title:Text("My traffic appbar"),
+    ),
+  ))
+);
