15a3535a484bfa874eec2d470402651f326987e3
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-21 23:37:46 +0530

    Ch6 - Safe area widget - pg104

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import "package:flutter/material.dart";
 
 void main()=>runApp(
   MaterialApp(home:Scaffold(
-    appBar: AppBar(
-      leading:Icon(Icons.traffic),
-      title:Text("My traffic appbar"),
+    body: SafeArea(
+      child: Text("Text widget inside safe area"),
     ),
   ))
 );
