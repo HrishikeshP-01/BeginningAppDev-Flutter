5a6e5874b5aa77780e4ff1cef76672c14586c86f
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-21 23:28:13 +0530

    Ch6 - Material app - pg101

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,9 @@
 import "package:flutter/material.dart";
-import "Person.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:VerticalSwipeExample())));
-
-List<Map> FetchPeople()
-{
-  return [
-    {"firstName":"Jim","lastName":"Halpert"}
-  ];
-}
-
-class VerticalSwipeExample extends StatelessWidget
-{
-  List<Map> objectPeople=FetchPeople();
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return ListView(
-      children: objectPeople.map(
-          (person)=>GestureDetector(
-            child:Person(person["firstName"],person["lastName"]),
-            onPanEnd:(e){print("Pan detected");}
-          )
-      ).toList(),
-    );
-  }
-}
+void main()=>runApp(MaterialApp(
+  home:Container(
+    child:Text("Material app example"),
+  ),
+  title:"Material App Title",
+  theme:ThemeData(primarySwatch:Colors.purple),
+));
