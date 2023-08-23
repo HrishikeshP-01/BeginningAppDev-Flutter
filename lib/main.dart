cb747dd22650e583d5f3c56a9803c9748aad920c
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-23 22:47:45 +0530

    Ch6 - CrossAxisAlignment.stretch - pg115

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:IntrinsicWidthExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:CrossAxisAlignmentExample())));
 
-class IntrinsicWidthExample extends StatelessWidget
+class CrossAxisAlignmentExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
+    // DONT FORGET TO ADD INTRINSIC WIDTH WHILE USING STRETCH
     return IntrinsicWidth(
       child:Column(
         mainAxisAlignment:MainAxisAlignment.center,
         crossAxisAlignment:CrossAxisAlignment.stretch,
-        children:<Text>[
-          Text("Hrishi"),
-          Text("Is"),
-          Text("Trying")
+        children:<Image>[
+          Image.asset("assets/images/Fig3.png"),
+          Image.asset("assets/images/Fig3.png"),
+          Image.asset("assets/images/Fig3.png")
         ],
       ),
     );
