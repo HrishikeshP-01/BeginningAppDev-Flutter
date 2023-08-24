dbce6352c8b73d17dfe7c32b95d999c1afebbefa
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-24 22:44:45 +0530

    Ch6 - Spacer widget - pg120

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,21 @@ void main()=>runApp(MaterialApp(home:Scaffold(body:ExpandedExample())));
 
 class ExpandedExample extends StatelessWidget
 {
-  /*MainAxisAlignment makes no difference because it's eaten up by Expanded*/
-  /*You can control how much each Expanded gets using the flex rating.
-  * Widgets with equal flex rating will have equal space*/
   @override
   Widget build(BuildContext context)
   {
     return Row(
       mainAxisAlignment:MainAxisAlignment.spaceAround,
       children:<Widget>[
-        Text("Hrishi"),
-        Expanded(flex:1,child:Text("Is")),
-        Expanded(flex:2,child:Text("Really")),
-        Expanded(flex:3,child:Text("Trying")),
+        Expanded(child:Text("Hrishi")),
+        Text("|"),
+        Spacer(),
+        Text("|"),
+        Expanded(child:Text("Is")),
+        Text("|"),
+        Spacer(),
+        Text("|"),
+        Expanded(child:Text("Trying"))
       ],
     );
   }
