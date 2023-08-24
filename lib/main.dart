7972a388ceabaf69df09f2a2c967c4d31e871e9f
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-24 22:32:04 +0530

    Ch6 - Flex factor in Expanded - pg119

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ void main()=>runApp(MaterialApp(home:Scaffold(body:ExpandedExample())));
 
 class ExpandedExample extends StatelessWidget
 {
+  /*MainAxisAlignment makes no difference because it's eaten up by Expanded*/
+  /*You can control how much each Expanded gets using the flex rating.
+  * Widgets with equal flex rating will have equal space*/
   @override
   Widget build(BuildContext context)
   {
@@ -11,9 +14,9 @@ class ExpandedExample extends StatelessWidget
       mainAxisAlignment:MainAxisAlignment.spaceAround,
       children:<Widget>[
         Text("Hrishi"),
-        Expanded(child: Text("Is")),
-        Expanded(child: Text("Really")),
-        Text("Trying"),
+        Expanded(flex:1,child:Text("Is")),
+        Expanded(flex:2,child:Text("Really")),
+        Expanded(flex:3,child:Text("Trying")),
       ],
     );
   }
