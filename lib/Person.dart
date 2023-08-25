27d1a847785ecce8197f465f4331e70bb387d9d9
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-25 23:44:09 +0530

    Ch6 - ListView.builder. Created Person class -pg123

--- lib/Person.dart
+++ lib/Person.dart
@@ -2,16 +2,12 @@ import "package:flutter/material.dart";
 
 class Person extends StatelessWidget
 {
-  final String firstName;
-  final String lastName;
-
-  Person(this.firstName,this.lastName){}
+  final String name;
+  Person(this.name){}
 
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      child:Text("$firstName $lastName")
-    );
+    return Text("$name");
   }
 }
