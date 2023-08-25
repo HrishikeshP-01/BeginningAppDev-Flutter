a5bc7c82465405b013affc8188914f6d94cec470
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-25 23:26:41 +0530

    Ch6 - Col ListView for static number of widgets -pg122

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ void main()=>runApp(MaterialApp(home:Scaffold(body:StaticListViewCol())));
 
 class StaticListViewCol extends StatelessWidget
 {
+  /*ListView static allows only widgets manually.
+  * This is used when you only have a few widgets to display.
+  * It's usually used to replace Rows & Columns when they can't contain widgets in screen*/
   @override
   Widget build(BuildContext context)
   {
-    return Text("Hi");
+    return ListView(
+      children:<Widget>[
+        Text("Hrishi"),
+        Text("Is"),
+        Text("Trying")
+      ],
+    );
   }
 }
