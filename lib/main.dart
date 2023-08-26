d6bf163b33dd0604848a6ab278c553ce7e37231e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-26 18:13:13 +0530

    Ch6 - GridView.extent - pg131

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:AlignAndCenter())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:GridViewExtentExample())));
 
-class AlignAndCenter extends StatelessWidget
+List<Image> getImages()
 {
+  List<Image> images=[];
+  for(int i=0;i<10;i++)
+    {
+      images.add(Image.asset("assets/images/Fig3.png"));
+    }
+  return images;
+}
+
+class GridViewExtentExample extends StatelessWidget
+{
+  /*GridView has auto scrolling & automatically populates widgets in a grid.
+  * GridView.extent constructor example.
+  * Extent specifies the max. width of the child.
+  * If they are bigger another widget is added to that row and all are shrunk till
+  * all fits & are below the max. width i.e., extent*/
   @override
   Widget build(BuildContext context)
   {
-    /*If you only want to align/center & don't want any other formatting:
-    * Use Align or Center widgets*/
-    return Column(
-      children:<Widget>[
-        Align(
-          alignment:Alignment.centerLeft,
-          child:Text("Hrishikesh")
-        ),
-        Center(
-          child:Text("P")
-        ),
-      ],
+    return GridView.extent(
+      maxCrossAxisExtent:300,
+      children:getImages(),
     );
   }
 }
