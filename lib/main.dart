4cde1ce25dd07c19e5c5137065bde06e221e4be7
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-26 17:17:46 +0530

    Ch6 - EdgeInsets - pg125

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:BoxModel())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:EdgeInsetsExample())));
 
-class BoxModel extends StatelessWidget
+class EdgeInsetsExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      margin:EdgeInsets.all(5.0),
-      padding:EdgeInsets.all(10.0),
-      decoration:BoxDecoration(border:Border.all(width:1.0)),
-      child: Image.asset("assets/images/Fig3.png")
+    return Column(
+      children:<Container>[
+        Container(
+          // Same value applied to all 4 sides
+          margin:EdgeInsets.all(5.0),
+          // Same value applied to opposite sides
+          padding:EdgeInsets.symmetric(horizontal:10.0,vertical:5.0),
+          decoration:BoxDecoration(border:Border.all(width:1.0)),
+          child:Image.asset("assets/images/Fig3.png")
+        ),
+        Container(
+          // Individual values applied to each side
+          margin:EdgeInsets.only(top:5.0,bottom:10.0,left:15.0,right:20.0),
+          // Values applied to LTRB respectively
+          padding:EdgeInsets.fromLTRB(5.0,10.0,15.0,20.0),
+          decoration:BoxDecoration(border:Border.all(width: 1.0)),
+          child:Image.asset("assets/images/Fig3.png"),
+        ),
+      ],
     );
   }
 }
