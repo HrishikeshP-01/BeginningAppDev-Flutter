996960848fb68ca2506fb2517274fcf340593bb0
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-26 18:25:43 +0530

    Ch6 - GridView.count - pg133

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,29 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:GridViewExtentExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:GridViewCountExample())));
 
 List<Image> getImages()
 {
   List<Image> images=[];
+
   for(int i=0;i<10;i++)
     {
       images.add(Image.asset("assets/images/Fig3.png"));
     }
   return images;
 }
-
-class GridViewExtentExample extends StatelessWidget
+class GridViewCountExample extends StatelessWidget
 {
-  /*GridView has auto scrolling & automatically populates widgets in a grid.
-  * GridView.extent constructor example.
-  * Extent specifies the max. width of the child.
-  * If they are bigger another widget is added to that row and all are shrunk till
-  * all fits & are below the max. width i.e., extent*/
   @override
   Widget build(BuildContext context)
   {
-    return GridView.extent(
-      maxCrossAxisExtent:300,
-      children:getImages(),
+    /*GridView.count() lets you specify how many widgets should be added in a row.
+    * The widgets are then resized accordingly.
+    * This ensures there are the same number of widgets in a row regardless of
+    * orientation*/
+    return GridView.count(
+      crossAxisCount: 2,
+      children: getImages(),
     );
   }
 }
