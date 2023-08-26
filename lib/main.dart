f8cb62a5387688c0131efe95d8f01743acad0f6b
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-26 17:46:06 +0530

    Ch6 - Container Alignment & Positioning - pg127

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:PaddingExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:AlignmentPositioning())));
 
-class PaddingExample extends StatelessWidget
+class AlignmentPositioning extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    // If you want only padding & no other formatting use Padding instead of Container
-    return Padding(
-      padding:EdgeInsets.all(5.0),
-      child:Image.asset("assets/images/Fig3.png")
+    /*When the container is bigger than the widget, you can align the widget
+    * either by specifying co-ordinates Alignment(0,0) - CENTER
+    * or using english words - Alignment.center, Alginment.centerLeft, etc.*/
+    return Column(
+      children:<Container>[
+        Container(
+          width:1500,
+          height:200,
+          alignment:Alignment(-1,-1),
+          child:Image.asset("assets/images/Fig3.png")
+        ),
+        Container(
+          width:1500,
+          height:200,
+          alignment:Alignment.centerRight,
+          child:Image.asset("assets/images/Fig3.png")
+        ),
+      ],
     );
   }
 }
