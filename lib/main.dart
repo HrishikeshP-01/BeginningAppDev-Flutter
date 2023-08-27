3aeccd6312ce38749b629271b74f5d1617e10e98
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-27 22:48:15 +0530

    Ch7 - Stack Navigation - pg140

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,21 @@
 import "package:flutter/material.dart";
+import "shopping.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:TableWidthExample())));
+void main()=>runApp(RoutingExample());
 
-class TableWidthExample extends StatelessWidget
+class RoutingExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    /*No matter what the contents of the table are each row & col are given equal space.
-    * However we can define how much space to give each row/col using the below method.
-    * Here 0th column is given 3% of available space & rest of the space is divided
-    * equally among the rest of the columns */
-    return Table(
-      columnWidths: {0: FractionColumnWidth(0.03)},
-      children: <TableRow>[
-        TableRow(
-          children:<Widget>[
-            Text("Sl. No.", style:TextStyle(fontWeight:FontWeight.bold)),
-            Text("Name",style:TextStyle(fontWeight:FontWeight.bold)),
-          ],
-        ),
-        TableRow(
-          children:<Widget>[
-            Text("1"),
-            Text("Jim"),
-          ],
-        ),
-        TableRow(
-          children:<Widget>[
-            Text("2"),
-            Text("Pam"),
-          ],
-        ),
-      ],
+    return MaterialApp(
+      title: "Shopping App",
+      initialRoute: "/",
+      routes: {
+        '/' : (BuildContext context)=>LandingScene(),
+        '/browse' : (BuildContext context)=>BrowsingScene(),
+        '/product': (BuildContext context)=>ViewProduct(),
+      }
     );
   }
 }
---
+++ lib/shopping.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+class LandingScene extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Text("Placeholder1");
+  }
+}
+
+class BrowsingScene extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Text("Placeholder2");
+  }
+}
+
+class ViewProduct extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Text("Placeholder3");
+  }
+}
