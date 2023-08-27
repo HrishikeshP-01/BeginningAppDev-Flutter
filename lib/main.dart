1f67b59aa1ac90918099fd7fb926b524c9cefa7e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-27 22:18:06 +0530

    Ch6 - Table widget - pg136

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,44 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:GridViewCountExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:TableExample())));
 
-List<Image> getImages()
-{
-  List<Image> images=[];
-
-  for(int i=0;i<10;i++)
-    {
-      images.add(Image.asset("assets/images/Fig3.png"));
-    }
-  return images;
-}
-class GridViewCountExample extends StatelessWidget
+class TableExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    /*GridView.count() lets you specify how many widgets should be added in a row.
-    * The widgets are then resized accordingly.
-    * This ensures there are the same number of widgets in a row regardless of
-    * orientation*/
-    return GridView.count(
-      crossAxisCount: 2,
-      children: getImages(),
+    /*GridView doesn't let us specify the rows & cols the widgets get added in.
+    * Using just rows & cols to make tables doesn't ensure that the alignment will be the same.
+    * AS the rows/cols don't communicate with each other & just display their respective widgets,
+    * the result would not be aligned like a table.
+    * Using a Table widget ensures that the rows & cols are algined.
+    * USE TABLE TO DISPLAY DATA & NOT FOR SETTING UP LAYOUT
+    * Tables are like tables of HTML not a good idea to define layout using it*/
+    return Table(
+      children:<TableRow>[
+        TableRow(
+          children: <Widget>[
+            Text("Salesperson", style:TextStyle(fontWeight: FontWeight.bold) ),
+            Text("January", style: TextStyle(fontWeight: FontWeight.bold)),
+            Text("February", style: TextStyle(fontWeight:FontWeight.bold)),
+          ],
+        ),
+        TableRow(
+          children: <Widget>[
+            Text("Jim"),
+            Text("8500"),
+            Text("8400"),
+          ],
+        ),
+        TableRow(
+          children: <Widget>[
+            Text("Dwight"),
+            Text("9500"),
+            Text("9000"
+                "")
+          ],
+        ),
+      ],
     );
   }
 }
