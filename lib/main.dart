0dd9d97c8234be2fea9f87c9e502c62453d62ac3
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-27 22:29:11 +0530

    Ch6 - Specifying width of columns in Table widget. End of Ch6 -pg137

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,35 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:TableExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:TableWidthExample())));
 
-class TableExample extends StatelessWidget
+class TableWidthExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    /*GridView doesn't let us specify the rows & cols the widgets get added in.
-    * Using just rows & cols to make tables doesn't ensure that the alignment will be the same.
-    * AS the rows/cols don't communicate with each other & just display their respective widgets,
-    * the result would not be aligned like a table.
-    * Using a Table widget ensures that the rows & cols are algined.
-    * USE TABLE TO DISPLAY DATA & NOT FOR SETTING UP LAYOUT
-    * Tables are like tables of HTML not a good idea to define layout using it*/
+    /*No matter what the contents of the table are each row & col are given equal space.
+    * However we can define how much space to give each row/col using the below method.
+    * Here 0th column is given 3% of available space & rest of the space is divided
+    * equally among the rest of the columns */
     return Table(
-      children:<TableRow>[
+      columnWidths: {0: FractionColumnWidth(0.03)},
+      children: <TableRow>[
         TableRow(
-          children: <Widget>[
-            Text("Salesperson", style:TextStyle(fontWeight: FontWeight.bold) ),
-            Text("January", style: TextStyle(fontWeight: FontWeight.bold)),
-            Text("February", style: TextStyle(fontWeight:FontWeight.bold)),
+          children:<Widget>[
+            Text("Sl. No.", style:TextStyle(fontWeight:FontWeight.bold)),
+            Text("Name",style:TextStyle(fontWeight:FontWeight.bold)),
           ],
         ),
         TableRow(
-          children: <Widget>[
+          children:<Widget>[
+            Text("1"),
             Text("Jim"),
-            Text("8500"),
-            Text("8400"),
           ],
         ),
         TableRow(
-          children: <Widget>[
-            Text("Dwight"),
-            Text("9500"),
-            Text("9000"
-                "")
+          children:<Widget>[
+            Text("2"),
+            Text("Pam"),
           ],
         ),
       ],
