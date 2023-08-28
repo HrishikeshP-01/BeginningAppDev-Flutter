821a0861fa8cc6f353b0169d78066c23d5cd179e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-28 22:23:57 +0530

    Ch7 - Filling the Drawer - pg149

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,61 @@
 import "package:flutter/material.dart";
+import "shopping.dart";
 
-void main()=>runApp(MaterialApp(home:DrawerHeaderExample()));
+void main()=>runApp(ShoppingCart());
 
-class DrawerHeaderExample extends StatelessWidget
+class ShoppingCart extends StatelessWidget
+{
+ @override
+ Widget build(BuildContext context)
+ {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(title:Text("Shopping App")),
+       body: LandingScene(),
+       drawer: ShoppingDrawer(),
+     ),
+     initialRoute: '/',
+     routes: {
+       '/browse': (BuildContext context) => BrowsingScene(),
+       '/product': (BuildContext context)=>ViewProduct(),
+       '/checkout': (BuildContext context)=>Checkout(),
+     },
+   );
+ }
+}
+
+class ShoppingDrawer extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Shopping App"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Stack(
-                children: <Widget>[
-                  Image.asset("assets/images/clanLogo.png"),
-                  Center(child:Text("My clan")),
-                ],
-              ),
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/images/clanLogo.png"),
+                Text("Clan Logo"),
+              ],
             ),
-            Text("Shopping Cart"),
-            Text("Products"),
-            Text("Checkout"),
-            Text("My Account")
-          ],
-        ),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text("Shopping Cart"),
+            onTap: (){Navigator.pushNamed(context, '/checkout');}
+          ),
+          ListTile(
+            leading: Icon(Icons.search),
+            title:Text("Browse"),
+            onTap: (){Navigator.pushNamed(context, '/browse');}
+          ),
+          ListTile(
+            leading: Icon(Icons.check_outlined),
+            title: Text("Checkout"),
+            onTap : (){Navigator.pushNamed(context, '/checkout');}
+          ),
+        ],
       ),
     );
   }
