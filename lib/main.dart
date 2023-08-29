f5d504f843e5ecb6ea4c25f1f28b309b1612889f
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-29 23:17:18 +0530

    Ch7 - Simple Dialog - pg156

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,27 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:DialogResponses()));
+void main()=>runApp(MaterialApp(home:SimpleDialogExample()));
 
-class DialogResponses extends StatelessWidget
+class SimpleDialogExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return ElevatedButton(
-      child: Text("Press me"),
-      onPressed: ()async{
-        // The builder returns the user's choice here
-        // Since it's Future<String> we await 'it' to convert it to String
-        var Response = await showDialog<String>(
-          context: context,
-          builder: (BuildContext context){
-            return AlertDialog(
-              content: Text("Did you eat?"),
-              actions:<Widget>[
-                ElevatedButton(
-                  child:Text("Yes"),
-                  onPressed:()=>Navigator.pop(context,"Yes"),
-                ),
-                ElevatedButton(
-                  child:Text("No"),
-                  onPressed:()=>Navigator.pop(context,"No"),
-                ),
+      child:Text("Press Me"),
+      onPressed: (){
+        showDialog(
+          context:context,
+          builder:(BuildContext context){
+            return SimpleDialog(
+              children: <Widget>[
+                Text("Hi"),
+                Text("Hrishi"),
+                Text("Here"),
               ],
             );
           }
         );
-        print(Response);
       }
     );
   }
