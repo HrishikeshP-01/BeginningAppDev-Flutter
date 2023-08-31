7da995ee3aae0f1eedd5bbc3627ba85aa6b72723
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-31 22:50:57 +0530

    Ch8 - Border.all() for container - pg171

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,20 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:BorderExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:BorderAllExample())));
 
-class BorderExample extends StatelessWidget
+class BorderAllExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Container(
-      width:300,
-      height: 200,
       decoration: BoxDecoration(
-        color: Colors.purple,
-        border: Border(
-          top: BorderSide(
-            width: 10,
-            color:Colors.red,
-          ),
-          left: BorderSide(
-            width: 5,
-            color: Colors.yellow,
-          ),
-          right: BorderSide(
-            width: 15,
-            color:Colors.grey,
-          ),
-          bottom: BorderSide(
-            width: 8,
-            color:Colors.black,
-          ),
-        ),
-      )
+        color: Colors.grey,
+        border: Border.all(
+          color:Colors.black,
+          width:10.0,
+        )
+      ),
     );
   }
 }
