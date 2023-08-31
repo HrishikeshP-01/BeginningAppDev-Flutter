28f13e9a814a6130f6d4fb2829988e0bcb1a256e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-31 23:01:22 +0530

    Ch8 - BorderRadius.all() - pg172

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:BorderRadiusExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:BorderRadiusOnlyExample())));
 
-class BorderRadiusExample extends StatelessWidget
+class BorderRadiusOnlyExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Container(
-      width: 300,
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10.0),
-          topRight: Radius.elliptical(5.0, 10.0),
-          bottomLeft: Radius.elliptical(5.0,10.0),
-          bottomRight: Radius.circular(10.0),
-        ),
+      width:300,
+      height:200,
+      decoration:BoxDecoration(
+        color:Colors.grey,
+        borderRadius:BorderRadius.all(Radius.circular(10.0)),
       ),
     );
   }
