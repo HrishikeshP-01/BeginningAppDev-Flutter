045d4b8d0d385209731ed4911e942ccd9bbcf3c8
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-31 22:57:40 +0530

    Ch8 - Different border radius for different corners - pg172

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:BorderAllExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:BorderRadiusExample())));
 
-class BorderAllExample extends StatelessWidget
+class BorderRadiusExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Container(
+      width: 300,
+      height: 200,
       decoration: BoxDecoration(
         color: Colors.grey,
-        border: Border.all(
-          color:Colors.black,
-          width:10.0,
-        )
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10.0),
+          topRight: Radius.elliptical(5.0, 10.0),
+          bottomLeft: Radius.elliptical(5.0,10.0),
+          bottomRight: Radius.circular(10.0),
+        ),
       ),
     );
   }
