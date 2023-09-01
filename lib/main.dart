dc3e48142a1586af626779185b93ce69eeb1e3b6
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-01 23:21:39 +0530

    Ch8 - Card widget - pg180

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:PositionedWidget())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:CardExample())));
 
-class PositionedWidget extends StatelessWidget
+class CardExample extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Card(
-      child: Stack(
-        children: <Widget>[
-          Image.asset("assets/images/Fig3.png"),
-          Positioned(
-            top:10, left: 10,
-            child:Text("Hrishikesh P", style:Theme.of(context).textTheme.headline2?.copyWith(color:Colors.orange)),
-          ),
-          Positioned(
-            bottom:10, right:10,
-            child:Text("hrishikeshpramodnair@gmail.com", style:Theme.of(context).textTheme.bodyText1?.copyWith(color:Colors.orange)),
-          )
-        ],
-      ),
+      elevation: 20.0,
+      child: Text("This is a card", style:Theme.of(context).textTheme.bodyText1?.copyWith(color:Colors.blue)),
     );
   }
 }
