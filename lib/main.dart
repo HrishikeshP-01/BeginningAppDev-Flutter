69a272263e6941cd14b5a7ca4fdca2fe9066252e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-01 23:14:37 +0530

    Ch8 - Positioned widgets - pg178

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:StackExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:PositionedWidget())));
 
-class StackExample extends StatelessWidget
+class PositionedWidget extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Card(
-      child:Stack(
+      child: Stack(
         children: <Widget>[
-          Image.asset("assets/images/clanLogo.png"),
-          Column(
-            children: <Widget>[
-              Text("Hrishikesh P",
-              style:Theme.of(context).textTheme.headline2?.copyWith(color:Colors.orange)),
-              //Expanded(child:Container()),
-              Text("hrishikeshpramodnair@gmail.com",
-              style:Theme.of(context).textTheme.bodyText1?.copyWith(color:Colors.orange)),
-            ],
+          Image.asset("assets/images/Fig3.png"),
+          Positioned(
+            top:10, left: 10,
+            child:Text("Hrishikesh P", style:Theme.of(context).textTheme.headline2?.copyWith(color:Colors.orange)),
           ),
+          Positioned(
+            bottom:10, right:10,
+            child:Text("hrishikeshpramodnair@gmail.com", style:Theme.of(context).textTheme.bodyText1?.copyWith(color:Colors.orange)),
+          )
         ],
       ),
     );
