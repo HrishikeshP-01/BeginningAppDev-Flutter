bb48b9fd76be4774f439efaee923e3ee27e56557
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-01 22:32:19 +0530

    Ch8 - Colors with opacity - pg175

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,19 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:SupermanShield())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:ColorWithOpacity())));
 
-class SupermanShield extends StatelessWidget
+class ColorWithOpacity extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Container(
-      child: CustomPaint(
-        size: Size(200,200),
-        painter: SupermanShieldPainter(),
+      width:300,
+      height:200,
+      child:Text("Hi there!"),
+      decoration:BoxDecoration(
+        color:Colors.red.withOpacity(0.5),
       ),
     );
   }
-}
-
-class SupermanShieldPainter extends CustomPainter
-{
-  @override
-  void paint(Canvas canvas,Size size)
-  {
-    canvas.drawPath(
-      Path()..moveTo(25,0)
-          ..lineTo(125,0)
-          ..lineTo(150,25)
-          ..lineTo(75,125)
-          ..lineTo(0,25)
-          ..lineTo(25,0),
-      Paint()
-        ..style=PaintingStyle.fill
-        ..color=Colors.red
-    );
-  }
-  @override
-  bool shouldRepaint(SupermanShieldPainter oldDelegate)=>false;
 }
