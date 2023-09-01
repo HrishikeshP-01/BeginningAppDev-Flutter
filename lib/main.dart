6ce17655f2ace3fb774281d8a89e710a1d594879
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-01 21:36:04 +0530

    Ch8 - Circle shape for containers - pg173

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:BorderRadiusOnlyExample())));
+void main()=>runApp(MaterialApp(home:Scaffold(body:ShapedContainers())));
 
-class BorderRadiusOnlyExample extends StatelessWidget
+class ShapedContainers extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Container(
-      width:300,
-      height:200,
-      decoration:BoxDecoration(
-        color:Colors.grey,
-        borderRadius:BorderRadius.all(Radius.circular(10.0)),
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color:Colors.orange,
+        shape:BoxShape.circle,
       ),
     );
   }
