af49a9063370fdd4b579a8de4a88c79f0685d4f3
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-02 16:08:56 +0530

    Ch8 - Material theme text styles - pg184

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(
-  title: "Ch8 - Theme",
-  theme: ThemeData(primarySwatch:Colors.yellow),
-  home: BodyWidget(),
-));
+void main()=>runApp(MaterialApp(home:Scaffold(body:TextUsingThemeExamples())));
 
-class BodyWidget extends StatelessWidget
+class TextUsingThemeExamples extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    return Card(
-      elevation: 20.0,
-      child: Padding(
-        child: Text("Hello there!"),
-        padding: EdgeInsets.all(10.0),
-      ),
+    return Column(
+      children: <Text>[
+        Text("headline1", style:Theme.of(context).textTheme.headline1),
+        Text("bodyText1", style:Theme.of(context).textTheme.bodyText1),
+        Text("bodyText2",style:Theme.of(context).textTheme.bodyText2),
+        Text("button",style:Theme.of(context).textTheme.button),
+        Text("caption",style:Theme.of(context).textTheme.caption),
+        Text("headline2",style:Theme.of(context).textTheme.headline2),
+        Text("headline3",style:Theme.of(context).textTheme.headline3),
+        Text("headline4",style:Theme.of(context).textTheme.headline4),
+        Text("subtitle1",style:Theme.of(context).textTheme.subtitle1),
+        Text("subtitle2",style:Theme.of(context).textTheme.subtitle2),
+        Text("overline",style:Theme.of(context).textTheme.overline),
+      ],
     );
   }
 }
