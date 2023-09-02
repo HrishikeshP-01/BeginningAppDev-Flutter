5053fda37ac49f63906edaa411ab49a6fb3b0bac
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-02 15:55:23 +0530

    Ch8 - Themes - pg181

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import "package:flutter/material.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:CardExample())));
+void main()=>runApp(MaterialApp(
+  title: "Ch8 - Theme",
+  theme: ThemeData(primarySwatch:Colors.yellow),
+  home: BodyWidget(),
+));
 
-class CardExample extends StatelessWidget
+class BodyWidget extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
     return Card(
       elevation: 20.0,
-      child: Text("This is a card", style:Theme.of(context).textTheme.bodyText1?.copyWith(color:Colors.blue)),
+      child: Padding(
+        child: Text("Hello there!"),
+        padding: EdgeInsets.all(10.0),
+      ),
     );
   }
 }
