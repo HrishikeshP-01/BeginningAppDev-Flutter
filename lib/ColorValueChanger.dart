f96558a993ac780306405aa200c7a453fd08fd95
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-03 17:18:45 +0530

    Ch9 - Explanation for ColorValueChanger - pg197

--- lib/ColorValueChanger.dart
+++ lib/ColorValueChanger.dart
@@ -1,5 +1,18 @@
 import "package:flutter/material.dart";
 
+/*We are going to use a single widget for all 3 sliders.
+* To optimize our performance we ensure only necessary parts are added in the setState fn
+* For example, the label of the slider once set doesn't change so it's not included in
+* setState. However, value needs to be updated & passed to the parent widget so it's in
+* setState.
+*
+* In flutter you can't pass data from a child to a parent. As a workaround we pass the
+* fn reference of a parent to a child & the function is then called by the child & the
+* data inside the child is passed as parameter. Therefore, the function of the parent is
+* run using child data.
+*
+* Here, we pass the reference of the setColor fn of parent and store it in onChanged var.
+* This is then run by the child & value is passed as parameter.*/
 class ColorValueChanger extends StatefulWidget
 {
   final String property;
