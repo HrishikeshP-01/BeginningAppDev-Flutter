09f74fb239918d1409f02f67dde9f5d0cdc640f1
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-03 16:56:30 +0530

    Ch9 - Explanation of passing data in StatefulWidgets -pg191

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import "package:flutter/material.dart";
 
 void main()=>runApp(MaterialApp(home:Scaffold(body:Foo())));
-
+/*You can't pass state from one widget to another. State can only exist within a widget.
+* However, you can pass data from parent widget to child widget.
+* How to pass data from StatefulWidget class to State class?
+* Flutter provides a variable - widget that represents the StatefulWidget class
+* This can be accessed by the State class & all the variables in the StatefulWidget
+* can be accessed via the State class.
+* In case of a StatelessWidget just use a constructor to pass values to child classes*/
 class Foo extends StatefulWidget
 {
   final String passedIn="Hello";
