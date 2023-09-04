5de9fab1d5c93cbe16e2c498b86fff2781a45166
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-04 22:24:12 +0530

    Ch9 - Created framework for ColorPicker -pg196

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import "package:flutter/material.dart";
+import "ColorCircle.dart";
+import "ColorValueChanger.dart";
 
-void main()=>runApp(MaterialApp(home:Scaffold(body:Foo())));
-/*You can't pass state from one widget to another. State can only exist within a widget.
-* However, you can pass data from parent widget to child widget.
-* How to pass data from StatefulWidget class to State class?
-* Flutter provides a variable - widget that represents the StatefulWidget class
-* This can be accessed by the State class & all the variables in the StatefulWidget
-* can be accessed via the State class.
-* In case of a StatelessWidget just use a constructor to pass values to child classes*/
-class Foo extends StatefulWidget
+void main()=>runApp(MaterialApp(home:Scaffold(body:ColorPicker())));
+
+class ColorPicker extends StatefulWidget
 {
-  final String passedIn="Hello";
   @override
-  _FooState createState()=>_FooState();
+  _ColorPickerState createState()=> _ColorPickerState();
 }
 
-class _FooState extends State<Foo>
+class _ColorPickerState extends State<ColorPicker>
 {
   @override
   Widget build(BuildContext context)
   {
-    return Text(widget.passedIn);
+    return Column(
+      children: <Widget>[
+
+      ],
+    );
   }
 }
