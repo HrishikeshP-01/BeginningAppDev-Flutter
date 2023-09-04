c857cb0ac7883b1114bc2c413e225fb4e3dee71c
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-04 22:37:38 +0530

    Ch9 - Explanation of _setColor - pg196

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,12 @@ class _ColorPickerState extends State<ColorPicker>
 
   void _setColor(String property, int value)
   {
+    /*We write this in the setState fn so as to ensure that whenever the variables
+    * changes the setState is called & the change is reflected on screen*/
     setState((){
+      /*We check if property name is same as the name of the color, if yes
+      * the value gets updated to whatever value the slider is at
+      * if no, the value stays the same*/
       _red=(property=="Red")?value:_red;
       _green=(property=="Green")?value:_green;
       _blue=(property=="Blue")?value:_blue;
