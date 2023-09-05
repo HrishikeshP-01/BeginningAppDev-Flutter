575c2775298aa2683719594789d85822bc17a320
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-05 23:23:16 +0530

    AppendixA - Different methods to iterate through a loop - pg289

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,12 @@
 * In dart list & array are the same.
 * Optional angular brackets <> shows the type*/
 List<dynamic> list = [1,"two",3.0];
+
+// Iterate through a list using for loop
+for(var d in list)
+{
+  print(d);
+}
+
+// Iterate through a list using for each loop
+list.foreach((d)=>print(d));
