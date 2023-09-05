e1129864b1c4cb3f7f516e656e1bb797e70f7325
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-05 23:18:07 +0530

    AppendixA - Dart data types - pg288

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,5 @@
-import "package:flutter/material.dart";
-import "package:path_provider/path_provider.dart";
+int x=10; // Integer
+double y = 2.0; // Double
+bool z = true; // Boolean
+String s = "hello"; // String
+dynamic d; // Dynamic. Dynamic variables can change types at any time. USE SPARINGLY
