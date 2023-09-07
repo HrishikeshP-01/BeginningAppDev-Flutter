a6c07731b7aaad496319b79b905a13cdd2adfcc3
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-07 22:49:13 +0530

    AppendixA - final keyword - pg292

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,1 @@
-/* Type Inference
-Dart will infer type based on value being assigned to it.*/
-
-var i = 12; // i is inferred as int
-i = 13; // Possible because i is int
-i = "X"; // Not possible because i is int
-var str = "X"; // str is inferred as string
+final int x = 10; // Final means that once value is assigned it can't be changed
