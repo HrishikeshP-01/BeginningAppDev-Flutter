88265652178270efaf8fa0eb86d3d717db808807
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-08 22:12:26 +0530

    AppendixA - Spread operator - pg294

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-// Multiline strings begin with 3 single/double quotes
-String name = """ hrishi 
-kesh 
-P
-""";
-
-String job = '''
-Data
-Engineer 
-''';
+// Spread operator ... will spread out elements of the array, flattening them
+List fiveToten = [5,6,7,8,9,10];
+// Spreading the inner array with "..."
+List numbers = [1,2,3,4,...fiveToten];
+// numbers now has => [1,2,3,4,5,6,7,8,9,10]
