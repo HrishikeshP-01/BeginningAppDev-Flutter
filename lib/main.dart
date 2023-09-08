664b268a5245568e2fc91efc762f77cef4827156
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-08 22:05:16 +0530

    AppendixA - Multiline string - pg294

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-String first = "";
-String last = "";
-String suffix = "";
+// Multiline strings begin with 3 single/double quotes
+String name = """ hrishi 
+kesh 
+P
+""";
 
-// This is string interpolation
-String fullName = "$first $last , $suffix";
-// is the same as
-fullName = first + " " + last + " , " + suffix;
-// When you use string interpolation for objects/dicts use {} or else compiler will get confused
-fullName = '${name['first']} ${name["last"] , ${name["suffix"]}';
+String job = '''
+Data
+Engineer 
+''';
