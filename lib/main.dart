863e167e99f386922108a501047c170b4a2ea835
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-08 22:20:56 +0530

    AppendixA - Map<foo,bar> -pg295

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-// Spread operator ... will spread out elements of the array, flattening them
-List fiveToten = [5,6,7,8,9,10];
-// Spreading the inner array with "..."
-List numbers = [1,2,3,4,...fiveToten];
-// numbers now has => [1,2,3,4,5,6,7,8,9,10]
+Map<String, dynamic>Person
+{
+  "first":"Ross",
+  "second":"Geller",
+  "dob":DateTime.parse("1972-07-16"),
+  "phno":999999999,
+}
