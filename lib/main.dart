9be789f88dc5cba9d607adb5d716f1fa1d2bc30e
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-09 22:40:58 +0530

    AppendixA - Positional fn parameters - pg296

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-/*
-Big/Fat arrow
-When you have a fn that RETURNS a value IN 1 LINE OF CODE
-you can put the returned value on the right side of => & argument list on the left
-The big arrow is syntactic sugar allowing devs to be more expressive with less code.*/
-
-int triple(int val)
+void sendEmail(String name, int phno)
 {
-  return val*3;
+  // code
 }
 
-Function triple2 = (int val)
-{
-  return val*3;
-};
-
-Function triple3 = (int val)=> val*3;
+sendEmail("Hrishi", 999);
