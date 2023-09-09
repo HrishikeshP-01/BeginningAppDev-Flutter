2df9221c7b2217a87b4bd5a2369c8fc30982a8d4
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-09 23:00:44 +0530

    AppendixA - Avoid this keyword within a class to refer to members of that class -pg297

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
-class Person
+class Name
 {
+  String first="";
+  String last="";
+  String initial="";
 
+  String getFullName()
+  {
+    /*In Dart the use of this keyword to refer to members of a class within a class is
+    * discouraged so as to keep code shorter & cleaner*/
+    // Don't use this
+    String no = this.first+this.last+this.initial;
+    // Do
+    String yes = first+last+initial;
+    return yes;
+  }
 }
-/*In Dart it's possible & encouraged to avoid using the new keyword while
-* instantiating a class*/
-// Don't use new
-Person no = new Person();
-// Do
-Person yes = Person();
