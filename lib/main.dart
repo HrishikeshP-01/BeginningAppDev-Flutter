591123cef2845eab1a31602227fead186d5f3d10
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-11 22:59:54 +0530

    AppendixA - Adding Mixins to a class -pg300

--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,19 @@ mixin Employment
     print("Calling boss");
   }
 }
+
+class Name
+{
+  String first;
+  String last;
+}
+
+/*Mixins are added to a class using the with keyword
+* Mixins are baskets of properties & methods that can be added to a class.
+* They look like classes but can't be instantiated like objects.
+* Here mixin Employement has been added to employee & all the vars & methods will be available*/
+
+class Employee extends Name with Employment
+{
+  String position;
+}
