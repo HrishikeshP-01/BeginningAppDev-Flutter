4f3331423ec5cbf02c8fee3bf445be9b2b8f5804
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-11 22:45:00 +0530

    AppendixA - Normal way of making constructor -pg289

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 class Name
 {
-  String first="";
-  String last="";
-  String initial="";
+  String first;
+  String last;
 
-  String getFullName()
+  Name(String first, String last)
   {
-    /*In Dart the use of this keyword to refer to members of a class within a class is
-    * discouraged so as to keep code shorter & cleaner*/
-    // Don't use this
-    String no = this.first+this.last+this.initial;
-    // Do
-    String yes = first+last+initial;
-    return yes;
+    this.first=first;
+    this.last=last;
   }
 }
