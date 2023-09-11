ea266e73e98807f0104e7ca21603c4be65610c27
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-11 22:48:49 +0530

    AppendixA - Class constructor parameter shorthand -p289

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ class Name
   String first;
   String last;
 
-  Name(String first, String last)
-  {
-    this.first=first;
-    this.last=last;
-  }
+  Name(this.first, this.last){}
+  /*Is equivalent to:
+  * Name(String first, String last)
+  * {
+  * this.first=first;
+  * this.last=last;
+  * }*/
 }
