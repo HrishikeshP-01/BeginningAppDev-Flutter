0b7381e24c3818349083af05d2601d29574bc2e9
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-11 23:15:22 +0530

    AppendixA - Cascade operator - pg300

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-// Dart doesn't support overloading
+class Person
+{
+  String first;
+  String last;
+  int id;
+}
+
+Person p = Person();
+p.id=100;
+p.first="H";
+p.last="P";
+p.save();
+
+/*Cascade operator
+* When you see 2 dots .. it means return this class but before you do
+* do the following with a property or method*/
+Person p1 = Person()..id=100..first="H"..last="P"..save();
