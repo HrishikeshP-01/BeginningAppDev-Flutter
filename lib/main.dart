a499a5590318f4f8e255d02535ab57872f3a53e6
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-12 22:56:24 +0530

    AppendixA - Named constructor - typical constructor - pg301

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,11 @@ class Person
 {
   String first;
   String last;
-  int id;
+
+  // Typical constructor
+  Person()
+  {
+    first="";
+    last = "";
+  }
 }
-
-Person p = Person();
-p.id=100;
-p.first="H";
-p.last="P";
-p.save();
-
-/*Cascade operator
-* When you see 2 dots .. it means return this class but before you do
-* do the following with a property or method*/
-Person p1 = Person()..id=100..first="H"..last="P"..save();
